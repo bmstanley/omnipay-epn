c64d62da5cad7dd661c7f8cc84cd93bdc3838695
Brian Stanley <bmstanley87@gmail.com>
2021-04-18 17:55:09 -0400

    More

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -2,7 +2,9 @@
 
 namespace Omnipay\eProcessingNetwork\Message;
 
+use Omnipay\Common\GatewayInterface;
 use Omnipay\Common\Message\AbstractRequest as OmnipayAbstractRequest;
+use Omnipay\Common\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 abstract class AbstractRequest extends OmnipayAbstractRequest
@@ -36,5 +38,49 @@ abstract class AbstractRequest extends OmnipayAbstractRequest
             'ePNAccount' => $this->getParameter('accountNumber'),
             'RestrictKey' => $this->getParameter('restrictKey'),
         ];
+    }
+
+
+
+    /**
+     * Get the merchant's eProcessingNetwork account number.
+     *
+     * @return string|null
+     */
+    public function getAccountNumber(): ?string
+    {
+        return $this->getParameter('accountNumber');
+    }
+
+    /**
+     * Set the merchant's eProcessingNetwork account number.
+     *
+     * @param string $accountNumber
+     * @return \Omnipay\Common\Message\RequestInterface
+     */
+    public function setAccountNumber(string $accountNumber): RequestInterface
+    {
+        return $this->setParameter('accountNumber', $accountNumber);
+    }
+
+    /**
+     * Get the merchant's secure code
+     *
+     * @return string|null
+     */
+    public function getRestrictKey(): ?string
+    {
+        return $this->getParameter('restrictKey');
+    }
+
+    /**
+     * Set the merchant's secure code
+     *
+     * @param string $restrictKey
+     * @return \Omnipay\Common\Message\RequestInterface
+     */
+    public function setRestrictKey(string $restrictKey): RequestInterface
+    {
+        return $this->setParameter('restrictKey', $restrictKey);
     }
 }
