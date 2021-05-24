43c18cd5513e4ce6bad78dd190a78570ecbac80a
Brian Stanley <bmstanley87@gmail.com>
2021-05-23 22:02:50 -0400

    more

--- src/Message/Concerns/HasCreditCardData.php
+++ src/Message/Concerns/HasCreditCardData.php
@@ -12,14 +12,17 @@ trait HasCreditCardData
      */
     protected function getCreditCardData(): array
     {
+        $month = str_pad(substr($this->getCard()->getExpiryMonth(), -2, 2), 2, '0', STR_PAD_LEFT);
+        $year = substr($this->getCard()->getExpiryYear(), -2, 2);
+
         return [
             'CardNo' => $this->getCard()->getNumber(),
             'CardNumber' => $this->getCard()->getNumber(),
             'CardType' => $this->getCard()->getBrand(),
-            'ExpMonth' => $this->getCard()->getExpiryMonth(),
-            'ExpireMonth' => $this->getCard()->getExpiryMonth(),
-            'ExpYear' => $this->getCard()->getExpiryYear(),
-            'ExpireYear' => $this->getCard()->getExpiryYear(),
+            'ExpMonth' => $month,
+            'ExpireMonth' => $month,
+            'ExpYear' => $year,
+            'ExpireYear' => $year,
             'CVV2Type' => '1',
             'CVV2' => $this->getCard()->getCvv(),
         ];
